75c03dad9b2f7c6426b3dc5caf1572189827eb6f
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2018-11-30 14:36:00 +0100

    A*

---
+++ Turret Man/Assets/Andrew_Stuff/A_Star/Node.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Just Some Variables And Methods That Is Needed To Calculate A*
+public class Node {
+
+	public bool NodeSearchedThrough = false;//if bool is used to check if the node have been searched through
+
+	public float[] PathfindingNodeID;//this array holds the move cost to each tile (wall , water, sand .....)
+	public Node[] NeighbourNodess;//neighbours for the nodes
+
+	public int PosX = 0;
+	public int PosY = 0;
+
+	public int MapCollision = 0;//this is used to set the correct id for the tile (wall, water, sand .....)
+	public float NodePathValue = 0;
+
+	int _XValue = 0, _YValue = 0; //just some values that im using
+
+	public Node _ParentNode = null;
+	public float GCost = 0;//GCost is the cost that have been used to get to this node
+	public float FCost = 0;//Gcost+Hcost
+	public int _HCost = 0;//HCost is how far away the end node is from this node
+
+
+	public Node(int posX, int posY, int placement) {
+
+		PosX = posX;
+		PosY = posY;
+
+		if (placement == 0) {//Corner
+			NeighbourNodess = new Node[3];
+		} else if (placement == 1) {//Side
+			NeighbourNodess = new Node[5];
+		} else {//Middle
+			NeighbourNodess = new Node[8];
+		}
+
+	}
+
+	public void SetStartNode(Node theParent, Node theEnd) {//setting parent gcost and hcost
+		NodeSearchedThrough = true;
+		_ParentNode = theParent;
+
+		_XValue = theEnd.PosX - PosX;
+		_YValue = theEnd.PosY - PosY;
+
+		if (_XValue < 0)
+			_XValue *= -1;
+		if (_YValue < 0)
+			_YValue *= -1;
+
+		_HCost = _XValue + _YValue;
+		GCost = 0;
+		FCost = _HCost;
+	}
+
+	public void SetParentAndHCost(Node theParent, Node theEnd, float[] pathnodeid) {//setting parent gcost and hcost
+		NodeSearchedThrough = true;
+		_ParentNode = theParent;
+
+		_XValue = theEnd.PosX - PosX;
+		_YValue = theEnd.PosY - PosY;
+
+
+		if (_XValue < 0)
+			_XValue *= -1;
+		if (_YValue < 0)
+			_YValue *= -1;
+
+		_HCost = _XValue + _YValue;
+
+		
+
+		if (theParent.PosX - PosX + theParent.PosY - PosY == 0 || theParent.PosX - PosX + theParent.PosY - PosY == 2 || theParent.PosX - PosX + theParent.PosY - PosY == -2) {
+			GCost = (pathnodeid[MapCollision] * 1.4f) + _ParentNode.GCost;
+		} else {
+			GCost = pathnodeid[MapCollision] + _ParentNode.GCost;
+		}
+		FCost = _HCost + GCost;
+	}
+
+	public void SetNewParent(Node theParent, float[] pathnodeid) {//Adding the parent GCost to this nodes gcost and adding the distance the parent had to travel to this node gcost
+
+		_ParentNode = theParent;
+		if (theParent.PosX - PosX + theParent.PosY - PosY == 0 || theParent.PosX - PosX + theParent.PosY - PosY == 2 || theParent.PosX - PosX + theParent.PosY - PosY == -2) {
+			GCost = (pathnodeid[MapCollision] * 1.4f) + _ParentNode.GCost;
+		} else {
+			GCost = pathnodeid[MapCollision] + _ParentNode.GCost;
+		}
+
+		FCost = _HCost + GCost;
+	}
+
+}
---
+++ Turret Man/Assets/Andrew_Stuff/A_Star/TankMan_WorldChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMan_WorldChanger {
+
+	//If The Maps Center Isnt 0,0, meaning 
+	public const float CenterX = 0;
+	public const float CenterY = 0;
+
+	//Needed For Nodes------------
+
+	public const int NodesInUnityMeter = 10;
+	public const float DistanceBetweenNodes = 0.125f;
+
+	public const int PathCostSize = 3;
+	public const int NodesSize = (int)(NodesInUnityMeter / DistanceBetweenNodes);
+	public const int NodesSizeHalf = (int)(NodesInUnityMeter / DistanceBetweenNodes) / 2;
+	public const int NodesTotal = NodesSize * NodesSize;
+
+	//-----------------------------
+
+
+
+}
