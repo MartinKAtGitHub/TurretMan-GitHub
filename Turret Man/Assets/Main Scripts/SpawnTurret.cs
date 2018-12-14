d8d8552aee1a435db2cbc75326bb45316af343d8
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2018-12-14 16:58:56 +0100

    fix test

--- Turret Man/Assets/Main Scripts/SpawnTurret.cs
+++ Turret Man/Assets/Main Scripts/SpawnTurret.cs
@@ -42,24 +42,18 @@ public class SpawnTurret : MonoBehaviour {
 
 			var playerX = Mathf.Floor((transform.position.x) / 0.5f) * 0.5f;//Player Snapped Node Position X
 			var playerY = Mathf.Floor((transform.position.y - offset) / 0.5f) * 0.5f;//Player Snapped Node Position Y (- offset) 
-
-			Debug.Log(transform.position.x / 0.5f % 0.5f) ;
-			Debug.Log(0 / 0.5f % 0.5f) ;
-			Debug.Log(0.24f / 0.5f % 0.5f) ;
-		  Debug.Log(	Mathf.Round(0.24f / 0.5f));
-			Debug.Log(0.25f / 0.5f % 0.5f) ;
-			Debug.Log(0.26f / 0.5f % 0.5f) ;
-			Debug.Log(0.5f / 0.5f % 0.5f);
-
-			Debug.Log(transform.position.x / 0.5f % 1);
-			if(transform.position.x / 0.5f % 1 < 0.5f) {
-
-			} else {
-
+	
+			if(transform.position.x / 0.5f % 1 < 0.5f) {//Increasing The Distance The Player Can Click To The Left (-1 == 2 nodes)
+				x = Mathf.Clamp(Mathf.Floor((mousPos.x) / 0.5f) * 0.5f, playerX - 1f, playerX + 0.5f);//Finding Mouse Node Position X, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+			} else {//Increasing The Distance The Player Can Click To The Right (+1 == 2 nodes)
+				x = Mathf.Clamp(Mathf.Floor((mousPos.x) / 0.5f) * 0.5f, playerX - 0.5f, playerX + 1f);//Finding Mouse Node Position X, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
 			}
 
-			 x = Mathf.Clamp(Mathf.Floor((mousPos.x) / 0.5f) * 0.5f, playerX - 2.5f, playerX + 2.5f);//Finding Mouse Node Position X, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
-			 y = Mathf.Clamp(Mathf.Floor((mousPos.y) / 0.5f) * 0.5f, playerY - 2.5f, playerY + 2.5f);//Finding Mouse Node Position Y, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+			if ((transform.position.y - offset) / 0.5f % 1 < 0.5f) {//Increasing The Distance The Player Can Click Down (-1 == 2 nodes)
+				y = Mathf.Clamp(Mathf.Floor((mousPos.y) / 0.5f) * 0.5f, playerY - 0.5f, playerY + 1f);//Finding Mouse Node Position Y, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+			} else {//Increasing The Distance The Player Can Click Up (+1 == 2 nodes)
+				y = Mathf.Clamp(Mathf.Floor((mousPos.y) / 0.5f) * 0.5f, playerY - 0.5f, playerY + 1f);//Finding Mouse Node Position Y, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+			}
 
 			Instantiate(GunTurretPrefab, new Vector3(x + 0.25f, y + 0.25f, 0), Quaternion.identity);
         //    Debug.Log("SpawnTurret");
@@ -85,8 +79,17 @@ public class SpawnTurret : MonoBehaviour {
 		var playerY = Mathf.Floor((transform.position.y - offset) / 0.5f) * 0.5f;//Player Snapped Node Position Y
 
 
-		 x = Mathf.Clamp(Mathf.Floor((mousPos.x) / 0.5f) * 0.5f, playerX - 2.5f, playerX + 2.5f);//Finding Mouse Node Position X, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
-		 y = Mathf.Clamp(Mathf.Floor((mousPos.y) / 0.5f) * 0.5f, playerY - 2.5f, playerY + 2.5f);//Finding Mouse Node Position Y, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+		if (transform.position.x / 0.5f % 1 < 0.5f) {//Increasing The Distance The Player Can Click To The Left (-1 == 2 nodes)
+			x = Mathf.Clamp(Mathf.Floor((mousPos.x) / 0.5f) * 0.5f, playerX - 1f, playerX + 0.5f);//Finding Mouse Node Position X, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+		} else {//Increasing The Distance The Player Can Click To The Right (+1 == 2 nodes)
+			x = Mathf.Clamp(Mathf.Floor((mousPos.x) / 0.5f) * 0.5f, playerX - 0.5f, playerX + 1f);//Finding Mouse Node Position X, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+		}
+
+		if ((transform.position.y - offset) / 0.5f % 1 < 0.5f) {//Increasing The Distance The Player Can Click Down (-1 == 2 nodes)
+			y = Mathf.Clamp(Mathf.Floor((mousPos.y) / 0.5f) * 0.5f, playerY - 0.5f, playerY + 1f);//Finding Mouse Node Position Y, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+		} else {//Increasing The Distance The Player Can Click Up (+1 == 2 nodes)
+			y = Mathf.Clamp(Mathf.Floor((mousPos.y) / 0.5f) * 0.5f, playerY - 0.5f, playerY + 1f);//Finding Mouse Node Position Y, Then Clamping It To Find The Most Left And Most Right Position The Mouse Can Be At
+		}
 		//	Debug.Log("X = ( " + x + ") Y = (" + y + ")");
 
 		GhostBox.transform.position = new Vector3(x + 0.25f, y + 0.25f, 0);//Visual TestBox
